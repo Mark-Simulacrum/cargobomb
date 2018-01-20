3ab816144168c85b56034222b9863f8285ed7e80
Mark Simulacrum <mark.simulacrum@gmail.com>
2018-01-19 18:05:57 -0700

    Remove sha search.
    The stored SHAs are from the downloaded repositories, and immediately
    after capturing them we copy those repositories, so there's a high
    chance that they exist. As such, we assume they do.

--- src/gh_mirrors.rs
+++ src/gh_mirrors.rs
@@ -28,6 +28,5 @@ pub fn fetch(url: &str) -> Result<()> {
 
 pub fn reset_to_sha(url: &str, sha: &str) -> Result<()> {
     let dir = &repo_dir(url)?;
-    git::shallow_fetch_sha(url, dir, sha)?;
     git::reset_to_sha(dir, sha)
 }
